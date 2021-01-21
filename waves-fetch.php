69562d7662e135c6f6e7179e8a327b1d023f5eb3
Morgan Leek <morganleek@gmail.com>
2021-01-21 09:41:31 +0800

    Init

---
+++ waves-fetch.php
@@ -0,0 +1,20 @@
+/*
+	Plugin Name:  Waves Fetch
+	Plugin URI:   https://github.com/morganleek/waves-fetch/
+	Description:  Buoy Monitoring Plugin
+	Version:      20200109
+	Author:       https://morganleek.me/
+	Author URI:   https://morganleek.me/
+	License:      GPL2
+	License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+	Text Domain:  wporg
+	Domain Path:  /languages
+	*/
+
+	// Security
+	defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+	// Version
+	define( 'UWA_VERSION', '20200112' );
+	// Paths
+	define( 'UWA__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+	define( 'UWA__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
