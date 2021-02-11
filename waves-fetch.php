35e2c6e6212b12af03270031eddb246ef817eed8
morganleek <morganleek@gmail.com>
2021-02-11 07:08:19 +0000

    Version update

--- waves-fetch.php
+++ waves-fetch.php
@@ -3,7 +3,7 @@
 	Plugin Name:  Waves Fetch
 	Plugin URI:   https://github.com/morganleek/waves-fetch/
 	Description:  WP Plugin for fetching buoy data via AWS
-	Version:      0.0.18
+	Version:      0.1.1
 	Author:       https://morganleek.me/
 	Author URI:   https://morganleek.me/
 	License:      GPL2
