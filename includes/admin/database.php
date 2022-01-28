9aaafd6ff91c79457ba9c4dc5af569f567c14e4f
morganleek <morganleek@gmail.com>
2022-01-28 04:13:40 +0000

    working spotter direct

--- includes/admin/database.php
+++ includes/admin/database.php
@@ -37,6 +37,7 @@
 			download_text TEXT NOT NULL,
 			description TEXT NOT NULL,
 			image VARCHAR(255) NOT NULL,
+			download_enabled MEDIUMINT(9) DEFAULT 1 NOT NULL
 		  PRIMARY KEY  (id)
 		) $charset_collate;";
 		dbDelta( $sql );
